4d7e27c4647836b5a34a77e119ff53421a4cbefb
snowflying233 <2936092243@qq.com>
2020-12-30 17:14:20 +0800

    update03

--- src/main/java/com/gongshu/TestPush.java
+++ src/main/java/com/gongshu/TestPush.java
@@ -3,5 +3,8 @@ package com.gongshu;
 public class TestPush {
     public static void main(String[] args) {
         System.out.println("new message!");
+        Person p = new Person();
+        p.setName("tom");
+        System.out.println(p.getName());
     }
 }
