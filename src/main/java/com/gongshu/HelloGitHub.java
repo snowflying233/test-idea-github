db755901f05b89b050a34d990c56a04ae749ffc0
snowflying233 <2936092243@qq.com>
2020-12-30 16:48:57 +0800

    update01

--- src/main/java/com/gongshu/HelloGitHub.java
+++ src/main/java/com/gongshu/HelloGitHub.java
@@ -3,5 +3,7 @@ package com.gongshu;
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("hello github! i'm from idea!");
+        System.out.println("test push! from idea!");
+        System.out.println("333333");
     }
 }
---
+++ src/main/java/com/gongshu/TestPush.java
@@ -0,0 +1,7 @@
+package com.gongshu;
+
+public class TestPush {
+    public static void main(String[] args) {
+        System.out.println("123321");
+    }
+}
