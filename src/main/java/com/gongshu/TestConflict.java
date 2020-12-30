dd1244d7efb970a091ac9000fc8dd05151f98b73
xjl-flying <xjldd13@163.com>
2020-12-30 21:16:50 +0800

    update "222222"

--- src/main/java/com/gongshu/TestConflict.java
+++ src/main/java/com/gongshu/TestConflict.java
@@ -6,5 +6,6 @@ package com.gongshu;
 public class TestConflict {
     public static void main(String[] args) {
         System.out.println("111111");
+        System.out.println("222222");
     }
 }
