1a24cae1f0b1a940ec9526f6960d550683853c2f
snowflying233 <2936092243@qq.com>
2020-12-30 17:17:11 +0800

    print "222"

--- src/main/java/com/gongshu/TestPush.java
+++ src/main/java/com/gongshu/TestPush.java
@@ -3,5 +3,6 @@ package com.gongshu;
 public class TestPush {
     public static void main(String[] args) {
         System.out.println("new message!");
+        System.out.println("222222");
     }
 }
